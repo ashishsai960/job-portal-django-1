65acb4e1701a48db74838ffea10bfd46acdbd9c4
Lakavath Ashish Sai Naik <96200707+ashishsai960@users.noreply.github.com>
2024-06-25 18:15:51 +0530

    TEST1

--- jobportal/settings.py
+++ jobportal/settings.py
@@ -83,14 +83,15 @@ WSGI_APPLICATION = 'jobportal.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME':'job_portal',
-        'USER':'postgres',
-        'PASSWORD':'7382252042',
-        'HOST':'localhost',
-        'PORT': '5431',
+        'NAME': config('DB_NAME', default='backend'),
+        'USER': config('DB_USER', default ='backend_user'),
+        'PASSWORD': config('DB_PASSWORD', default='12345'),
+        'HOST': config('DB_HOST', default='localhost'),
+        'PORT': config('DB_PORT', default='5432'),
     }
 }
 
