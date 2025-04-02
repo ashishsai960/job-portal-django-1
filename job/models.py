2723c6fdd0dc1e514f2053c87e63535c7a4effea
Lakavath Ashish Sai Naik <96200707+ashishsai960@users.noreply.github.com>
2025-04-02 10:31:13 +0530

    Update models.py

--- job/models.py
+++ job/models.py
@@ -58,7 +58,7 @@
 #     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='notifications')
 #     message = models.TextField()
 #     created_at = models.DateTimeField(auto_now_add=True)
-#     is_read = models.BooleanField(default=False)
+
 
 
 from django.contrib.auth.models import AbstractUser
